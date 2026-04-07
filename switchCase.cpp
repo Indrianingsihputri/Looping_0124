02ef03c38d151c07880a76a35d387c92a7676144
“indrianingsihputri” <indrianingsihputri30@gmail.com>
2026-04-07 10:49:22 +0700

    menambahkan void input

--- switchCase.cpp
+++ switchCase.cpp
@@ -3,3 +3,14 @@ using namespace std;
 
 int KodeJurusan;
 string namaJurusan, statusKuota;
+
+void input()
+{
+    cout << "===PENDAFTARAN JURUSAN===" << endl;
+    cout << "1. Teknik Informatika" << endl;
+    cout << "2. Teknik Elektro" << endl;
+    cout << "3. Sistem Informasi" << endl;
+    cout << "-------------------------------------" << endl;
+    cout << "Masukkan kode jurusan (1-3) = ";
+    cin >> KodeJurusan;
+}
